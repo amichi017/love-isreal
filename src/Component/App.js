665ee0a768795a847b43b212653610fd9edbaec7
amichi017 <amichi12345678@gmail.com>
2020-10-06 15:57:47 +0300

    first

---
+++ src/Component/App.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import AppBar from '@material-ui/core/AppBar';
+
+import Toolbar from '@material-ui/core/Toolbar';
+
+import IconButton from '@material-ui/core/IconButton';
+
+import Fab from '@material-ui/core/Fab';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
+const useStyles = makeStyles({
+  list: {
+    width: 200,
+   
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+export default function App() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['בית כנסת', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      
+    <AppBar position="fixed" color="inherit" className={classes.appBar}>
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="open drawer">
+        <MenuIcon />
+      </IconButton>
+      <Fab color="secondary" aria-label="add" className={classes.fabButton}>
+        <AddIcon />
+      </Fab>
+      <div className={classes.grow} />
+      <IconButton color="inherit">
+        <SearchIcon />
+      </IconButton>
+      <IconButton edge="end" color="inherit">
+        <MoreIcon />
+      </IconButton>
+    </Toolbar>
+  </AppBar>
+    <div>
+      
+      {['left', 'right', 'top', 'bottom'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+    </div>
+  );
+}
+
+
