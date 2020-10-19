38b6922b1918071630bfa2509fa13a86734a1fe5
amichi017 <amichi12345678@gmail.com>
2020-10-19 14:02:14 +0300

    deploy frontend

---
+++ frontend/build/precache-manifest.d832bfe33928466c97a5389b7ad5e666.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6ea5949bb7c17421120efa9c04371f",
+    "url": "/israel/frontend/index.html"
+  },
+  {
+    "revision": "019ce03ffa2b1d919a07",
+    "url": "/israel/frontend/static/css/2.69507427.chunk.css"
+  },
+  {
+    "revision": "546ce2cf5efbf1df349f",
+    "url": "/israel/frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "019ce03ffa2b1d919a07",
+    "url": "/israel/frontend/static/js/2.59d0ee85.chunk.js"
+  },
+  {
+    "revision": "f3a6a51dd260c4f924bf796e7b33f546",
+    "url": "/israel/frontend/static/js/2.59d0ee85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "546ce2cf5efbf1df349f",
+    "url": "/israel/frontend/static/js/main.1834da75.chunk.js"
+  },
+  {
+    "revision": "defd75c6cd3afc405423",
+    "url": "/israel/frontend/static/js/runtime-main.9b57b1b7.js"
+  },
+  {
+    "revision": "78c18ff44827752861d078fd99820253",
+    "url": "/israel/frontend/static/media/bg.78c18ff4.jpg"
+  },
+  {
+    "revision": "225b4ae91b884682ec187d5b77e43b50",
+    "url": "/israel/frontend/static/media/slide_2.225b4ae9.jpg"
+  },
+  {
+    "revision": "039f54b71762dfb40b264bda0cc204ee",
+    "url": "/israel/frontend/static/media/slide_3.039f54b7.jpg"
+  },
+  {
+    "revision": "41436d0552fdf8498ac4e434e27365f0",
+    "url": "/israel/frontend/static/media/slide_4.41436d05.jpg"
+  }
+]);
