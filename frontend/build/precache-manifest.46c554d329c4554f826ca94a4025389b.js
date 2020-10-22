7b962310ad5d29f0d09a93d1fbfec5a6f2c7b65c
amichi017 <amichi12345678@gmail.com>
2020-10-22 21:49:46 +0300

    fix main screen

---
+++ frontend/build/precache-manifest.46c554d329c4554f826ca94a4025389b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ea37dfe65a9e9ed03f9d46351c708e",
+    "url": "/love-isreal/index.html"
+  },
+  {
+    "revision": "4ad27b060c9a2a0ec053",
+    "url": "/love-isreal/static/css/2.69507427.chunk.css"
+  },
+  {
+    "revision": "9eca9eec6af30f500d41",
+    "url": "/love-isreal/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ad27b060c9a2a0ec053",
+    "url": "/love-isreal/static/js/2.7ef66c3e.chunk.js"
+  },
+  {
+    "revision": "fb1309f4feab98899f64475ff2cdc726",
+    "url": "/love-isreal/static/js/2.7ef66c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206993b4e94dbe220c43",
+    "url": "/love-isreal/static/js/3.f67b08ca.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/love-isreal/static/js/3.f67b08ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684ab1672a33105ae651",
+    "url": "/love-isreal/static/js/4.89fddc37.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/love-isreal/static/js/4.89fddc37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b644adb773d80f4e788f",
+    "url": "/love-isreal/static/js/5.038fe1e6.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/love-isreal/static/js/5.038fe1e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eca9eec6af30f500d41",
+    "url": "/love-isreal/static/js/main.90664a30.chunk.js"
+  },
+  {
+    "revision": "3a86b3573cb7fb8e7e65",
+    "url": "/love-isreal/static/js/runtime-main.7eea5dae.js"
+  },
+  {
+    "revision": "78c18ff44827752861d078fd99820253",
+    "url": "/love-isreal/static/media/bg.78c18ff4.jpg"
+  },
+  {
+    "revision": "225b4ae91b884682ec187d5b77e43b50",
+    "url": "/love-isreal/static/media/slide_2.225b4ae9.jpg"
+  },
+  {
+    "revision": "039f54b71762dfb40b264bda0cc204ee",
+    "url": "/love-isreal/static/media/slide_3.039f54b7.jpg"
+  },
+  {
+    "revision": "41436d0552fdf8498ac4e434e27365f0",
+    "url": "/love-isreal/static/media/slide_4.41436d05.jpg"
+  },
+  {
+    "revision": "c530fe596fce51d40c7bb1f576f30977",
+    "url": "/love-isreal/static/media/torah.c530fe59.svg"
+  }
+]);
