d0f05c3988b06191345bd005fa4febb0e7684071
amichi017 <amichi12345678@gmail.com>
2020-11-15 00:17:26 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/love-isreal/precache-manifest.7c7f982c07c659aa9e9c417fb7c90dfc.js"
+  "/love-isreal/precache-manifest.7d86b2f254cd49a51a7133763834e3eb.js"
 );
 
 self.addEventListener('message', (event) => {
